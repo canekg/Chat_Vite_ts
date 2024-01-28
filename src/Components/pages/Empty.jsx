3d5edbd13b2903f150c742a2b764f5f9adc49fc8
Alexandr <canekg@mail.ru>
2024-01-28 23:23:06 +0500

    added pages

---
+++ src/Components/pages/Empty.jsx
@@ -0,0 +1,21 @@
+import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
+import routes from '../../routes';
+import empty from '../../assets/404.png';
+
+const EmptyPage = () => {
+  const { t } = useTranslation('translation');
+  return (
+    <div className="text-center w-100">
+      <img src={empty} alt={t('emptyPage.emptyPage')} className="img-fluid rounded mx-auto d-block" id="error" />
+      <h1 className="h4 text-muted">{t('notFound')}</h1>
+      <p className="text-muted">
+        {t('returnToHome')}
+        {' '}
+        <Link to={routes.home()}>{t('toMain')}</Link>
+      </p>
+    </div>
+  );
+};
+
+export default EmptyPage;
---
+++ src/Components/pages/Main.jsx
@@ -0,0 +1,48 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { toast } from 'react-toastify';
+import { useTranslation } from 'react-i18next';
+import Channels from '../chat/Channels';
+import Messages from '../chat/Messages';
+import { useAuth } from '../../context/AuthProvider';
+import getDataChannels from '../../api/getDataChannels';
+import getModalComponent from '../Modals/index';
+import { getTypeModal } from '../../selectors/index.js';
+
+const MainPage = () => {
+  const { t } = useTranslation();
+  const auth = useAuth();
+  const dispatch = useDispatch();
+  const { header } = auth;
+  const type = useSelector(getTypeModal);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        dispatch(getDataChannels(dispatch, header));
+      } catch (error) {
+        if (error.response && error.response.status === 401) {
+          toast.error(t('notifications.notАuthorized'));
+          auth.logOut();
+        } else {
+          toast.error(t('notifications.another'));
+        }
+      }
+    };
+
+    fetchData();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [dispatch]);
+
+  return (
+    <div className="container h-100 my-4 overflow-hidden rounded shadow">
+      <div className="row h-100 bg-white flex-md-row">
+        <Channels />
+        <Messages />
+        { getModalComponent(type) }
+      </div>
+    </div>
+  );
+};
+export default MainPage;
