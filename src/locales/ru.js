18088eb082723f6fe95d523b9ac31cdb6982ab61
Alexandr <canekg@mail.ru>
2024-01-28 23:22:37 +0500

    added locales

---
+++ src/locales/ru.js
@@ -0,0 +1,77 @@
+export default {
+  translation: {
+    language: 'Русский',
+    logOut: 'Выйти',
+    notFound: 'Страница не найдена',
+    loginHeader: 'Войти',
+    username: 'Ваш ник',
+    password: 'Пароль',
+    confirmPassword: 'Подтвердите пароль',
+    authFailed: 'Неверные имя пользователя или пароль',
+    userName: 'Имя пользователя',
+    noAccountQM: 'Нет аккаунта? ',
+    registration: 'Регистрация',
+    register: 'Зарегистрироваться',
+    mainHeader: 'Hexlet Chat',
+    returnToHome: 'Но вы можете перейти ',
+    channels: 'Каналы',
+    newMessage: 'Новое сообщение',
+    newMessagePlaceholder: 'Введите сообщение...',
+    send: 'Отправить',
+    toMain: 'на главную страницу',
+
+    dropdown: {
+      control: 'Управление каналом',
+      remove: 'Удалить',
+      rename: 'Переименовать',
+    },
+
+    placeholders: {
+      login: 'Ваш ник',
+      username: 'Имя пользователя',
+      password: 'Пароль',
+      confirmPassword: 'Подтвердите пароль',
+      newMessage: 'Введите сообщение...',
+    },
+
+    validation: {
+      userExists: 'Такой пользователь уже существует',
+      authFailed: 'Неверные имя пользователя или пароль',
+      minMaxsimSymbols: 'От 3 до 20 символов',
+      emptyField: 'обязательное поле',
+      minLengthPassword: 'Не менее 6 символов',
+      passwordMatch: 'Пароли должны совпадать',
+      uniqueness: 'Должно быть уникальным',
+    },
+
+    notifications: {
+      errMessage: 'Что то пошло не так, при отправке сообщения на сервер',
+      another: 'Что то пошло не так',
+      notАuthorized: 'Ошибка Авторизации',
+      addChannel: 'Канал создан',
+      removeChannel: 'Канал удалён',
+      renameChannel: 'Канал переименован',
+      errorAddChannel: 'Возникла ошибка при создании канала',
+      errorRemoveChannel: 'Возникла ошибка при удалении канала',
+      errorRenameChannel: 'Возникла ошибка при переименовании канала',
+    },
+
+    messagesCounter: {
+      messages_zero: '{{count}} сообщений',
+      messages_one: '{{count}} сообщение',
+      messages_few: '{{count}} сообщения',
+      messages_many: '{{count}} сообщений',
+    },
+
+    modal: {
+      addChannel: 'Добавить канал',
+      removeChannel: 'Удалить канал',
+      renameChannel: 'Переименовать канал',
+      remove: 'удалить',
+      send: 'отменить',
+      cancel: 'отправить',
+      confirmSure: 'Уверены ?',
+      channelName: 'Имя канала',
+    },
+  },
+};
