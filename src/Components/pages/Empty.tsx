993328ec836ef804c5fd8617900825038d54c899
Alexandr <canekg@mail.ru>
2024-01-30 21:08:15 +0500

    JavaScript to TypeScript migration file Empty

---
+++ src/Components/pages/Empty.tsx
@@ -0,0 +1,25 @@
+import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
+import React from 'react';
+import routes from '../../routes.ts';
+import empty from '../../assets/404.png';
+
+const EmptyPage: React.FC = () => {
+  const { t } = useTranslation('translation');
+  return (
+    <div className='text-center w-100'>
+      <img
+        src={empty}
+        alt={t('emptyPage.emptyPage')}
+        className='img-fluid rounded mx-auto d-block'
+        id='error'
+      />
+      <h1 className='h4 text-muted'>{t('notFound')}</h1>
+      <p className='text-muted'>
+        {t('returnToHome')} <Link to={routes.home()}>{t('toMain')}</Link>
+      </p>
+    </div>
+  );
+};
+
+export default EmptyPage;
