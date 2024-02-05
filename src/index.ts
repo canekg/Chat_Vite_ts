d699032c0311c1ae51817f73b9d1c360b79e5a30
Alexandr <canekg@mail.ru>
2024-02-05 21:53:19 +0500

    typing completed (Typescript) src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
-// import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
 import init from "./init";
 
 const render = async () => {
   const root = ReactDOM.createRoot(
-    document.getElementById("chat")
+    document.getElementById("chat") as HTMLElement
   );
   root.render(await init());
 };
